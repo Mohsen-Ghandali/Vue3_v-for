e5aa25231fef8f2375e12aa0efedf7c15d218adf
Mohsen Ghandali <mohsen.2527.gmail.com>
2023-09-09 11:00:56 +0200

    Commit-4

--- app.js
+++ app.js
@@ -4,11 +4,32 @@ const app = Vue.createApp({
         return {
             showContainer: true,
             users: [
-                { userName: "alex123", name: "Alex Müller", email: "alex@test.com", tel: "0049 162 0780 7890",link:"https://google.com" },
+                {
+                    userName: "alex123",
+                    name: "Alex Müller",
+                    email: "alex@test.com",
+                    tel: "0049 162 0780 7890",
+                    image: "./images/avatar.png",
+                    link: "https://google.com"
+                },
 
-                { userName: "martin007", name: "Martin Schmidt", email: "martin@test.com", tel: "0049 176 4564 7897",link:"https://yahoo.com" },
-                
-                { userName: "luis-m", name: "Luis Max", email: "luis@test.com", tel: "0049 000 2495 8748",link:"https://youtube.com" }
+                {
+                    userName: "martin007",
+                    name: "Martin Schmidt",
+                    email: "martin@test.com",
+                    tel: "0049 176 4564 7897",
+                    image: "./images/avatar2.png",
+                    link: "https://yahoo.com"
+                },
+
+                {
+                    userName: "luis-m",
+                    name: "Luis Max",
+                    email: "luis@test.com",
+                    tel: "0049 000 2495 8748",
+                    image: "./images/avatar3.png",
+                    link: "https://youtube.com"
+                }
             ]
         };
     },
